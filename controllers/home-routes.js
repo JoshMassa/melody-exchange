78fdd9e9999321505a2b97288d540ff6debda3d2
plucafo <plucaf87@gmail.com>
2024-04-01 20:23:51 -0700

    handlebars views set up and working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-    res.send("Hello, World!");
+    res.render('home');
 })
 
 module.exports = router;
--- models/Product.js
+++ models/Product.js
@@ -45,7 +45,7 @@ Product.init(
     user_id: {
         type: DataTypes.INTEGER,
         references: {
-            model: 'User',
+            model: 'user',
             key: 'id',
         }
     },
