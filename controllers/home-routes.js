54a5c00be3d5938c4ddf3702612d6bbb29849939
plucafo <plucaf87@gmail.com>
2024-04-06 10:38:44 -0700

    passed in req.session.logged_in user data to the home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,6 +26,7 @@ router.get('/', withAuth, async (req, res) => {
 
         res.render('home', {
             posts,
+            logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
