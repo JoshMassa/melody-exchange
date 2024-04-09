9e94588c0d152cd9693a7d24b3591bd910a25c7b
Josh Massa <joshmassa@gmail.com>
2024-04-08 20:20:29 -0400

    Working on implementing image upload feature - not yet done, do not merge with develop

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,19 +1,48 @@
 const router = require('express').Router();
 const { Category, Post, Product, User} = require('../../models');
 const withAuth = require('../../utils/auth');
+const multer = require('multer');
+const path = require('path');
+const express = require('express');
+const uploadsFolderPath = 'uploads';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
 
 // Create a new blog post
-router.post('/', withAuth, async (req, res) => {
+router.post('/', withAuth, upload.single('image'), async (req, res) => {
     try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'File upload failed' });
+        }
+        
+        console.log("Request file:", req.file);
+        const { title, content } = req.body;
+        const imagePath = req.file ? path.join(uploadsFolderPath, req.file.filename) : null;
+        
         const newPost = await Post.create({
-            ...req.body,
+            title,
+            content,
+            image: imagePath,
             user_id: req.session.user_id,
         });
-
+        
         res.status(200).json(newPost);
     } catch (err) {
+        console.error("Error:", err);
         res.status(500).json(err);
     }
 });
+
+// Defining a route to serve uploaded images
+router.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 module.exports = router;
