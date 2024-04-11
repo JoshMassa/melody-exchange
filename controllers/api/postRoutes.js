94e9f5d9e5c81159ec4ec2844d2e172f2015ca27
Josh Massa <joshmassa@gmail.com>
2024-04-11 18:29:36 -0400

    Changed out multer package for cloudinary package

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,25 +1,22 @@
 const router = require('express').Router();
 const { Category, Post, Product, User, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
-const multer = require('multer');
-
-const upload = multer({ dest: "./public/uploads/" });
 
 // Create a new blog post
-router.post('/', withAuth, upload.single('image'), async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
-        if (!req.file) {
-            return res.status(400).json({ error: 'File upload failed' });
+        const { title, price, content, category, imageUrl } = req.body;
+
+        if (!title || !price || !content || !category || !imageUrl) {
+            res.status(400).json({ message: 'All fields must be provided!' });
+            return;
         }
-        
-        const { title, price, content, category } = req.body;
-        const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
         
         const newPost = await Post.create({
             title,
             price,
             content,
-            image: imagePath,
+            image: imageUrl,
             user_id: req.session.user_id,
             category_id: category
         });
