2e290fbd65a6b66a1d18a7cd3b18efd20b7873d6
Josh Massa <joshmassa@gmail.com>
2024-04-11 19:50:19 -0400

    Fixed Heroku new post bug

--- models/Category.js
+++ models/Category.js
@@ -11,7 +11,6 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
     },
     category_name: {
       type: DataTypes.STRING,
